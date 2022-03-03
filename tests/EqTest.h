e30cdf4d918488373240a05405494912435e41bb
KIvan1 <kirillov20403@gmail.com>
2022-03-03 14:06:08 +0300

    first commit

---
+++ tests/EqTest.h
@@ -0,0 +1,46 @@
+#ifndef EQTEST_H
+#define EQTEST_H
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+extern "C"{
+#include "SolveEquation.h"
+}
+
+TEST(noroots_TEST, suite1)
+{
+    double x1, x2;
+    int c = SolveEq(0, 0, 3, &x1, &x2);
+    EXPECT_EQ(c, 1);
+}
+
+TEST(noroots_TEST, suite2)
+{
+    double x1, x2;
+    int c = SolveEq(1, 2, 3, &x1, &x2);
+    EXPECT_EQ(c, 1);
+}
+
+TEST(oneroot_TEST, suite1)
+{
+    double x1, x2;
+    SolveEq(0, 2, 4, &x1, &x2);
+    EXPECT_EQ(x1, 2);
+}
+
+TEST(oneroots_TEST, suite2)
+{
+    double x1, x2;
+    SolveEq(1, 2, 1, &x1, &x2);
+    EXPECT_EQ(x1, -1);
+}
+
+TEST(tworoots_TEST, suite1)
+{
+    double x1, x2;
+    SolveEq(1, 2, -3, &x1, &x2);
+    EXPECT_EQ(x1, 1);
+    EXPECT_EQ(x2, -3);
+}
+
+#endif // EQTEST_H
