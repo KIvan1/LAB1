2381eb0b7920defc8a251169e40aa50a9f9319d2
KIvan1 <kirillov20403@gmail.com>
2022-03-04 11:50:14 +0300

    commit

--- tests/EqTest.h
+++ tests/EqTest.h
@@ -9,35 +9,40 @@ extern "C"{
 
 TEST(noroots_TEST, suite1)
 {
-    double x1, x2;
+    double x1;
+    double x2;
     int c = SolveEq(0, 0, 3, &x1, &x2);
     EXPECT_EQ(c, 1);
 }
 
 TEST(noroots_TEST, suite2)
 {
-    double x1, x2;
+    double x1;
+    double x2;
     int c = SolveEq(1, 2, 3, &x1, &x2);
     EXPECT_EQ(c, 1);
 }
 
 TEST(oneroot_TEST, suite1)
 {
-    double x1, x2;
+    double x1;
+    double x2;
     SolveEq(0, 2, 4, &x1, &x2);
     EXPECT_EQ(x1, 2);
 }
 
 TEST(oneroots_TEST, suite2)
 {
-    double x1, x2;
+    double x1;
+    double x2;
     SolveEq(1, 2, 1, &x1, &x2);
     EXPECT_EQ(x1, -1);
 }
 
 TEST(tworoots_TEST, suite1)
 {
-    double x1, x2;
+    double x1;
+    double x2;
     SolveEq(1, 2, -3, &x1, &x2);
     EXPECT_EQ(x1, 1);
     EXPECT_EQ(x2, -3);
