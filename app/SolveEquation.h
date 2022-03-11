09222f6d912a5b57be65d5fbdf3b97c49a24540f
KIvan1 <kirillov20403@gmail.com>
2022-03-11 09:53:25 +0300

    commit

--- app/SolveEquation.h
+++ app/SolveEquation.h
@@ -2,6 +2,8 @@
 #define SOLVEEQUATION_H
 
 int SolveEq(double a, double b, double c, double* x1, double* x2);
+
+
 double SolveL(double b, double c);
 double FindFR(double D, double a, double b);
 double FindSR(double D, double a, double b);
